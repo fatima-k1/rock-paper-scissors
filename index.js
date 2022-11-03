5561bdc8e2fb8a02d12ab2eb45efccb6f449da2a
fatima-k1 <khandwalla.f@gmail.com>
2022-11-03 17:18:34 +0300

    wrote pseudocode

---
+++ index.js
@@ -0,0 +1,18 @@
+//Record the round number(3 rounds) the player is on. Start it on 1
+//Provide the player with a way to choose between rock, paper or scissors
+//The player will play against the computer so return either rock,paper or scissors
+//Once a round ends, record it somewhere so the player can see the results of the previous rounds
+//Next, check who won that round
+//If the player or the computer won:
+  //1. Display "Win" and "Lose" respectively
+  //2. Let the player choose again and return one of the 3 again
+  //3. Increment the round number by 1
+//If the player won 2 or all rounds:
+  //1. Display congratulations message
+  //2. Stop the player from being able to choose again
+  //3. Display control allowing the player to restart the game
+//If the computer won 2 or all rounds:
+  //1. Tell the player it is game over
+  //2. Repeat steps 2 and 3 above
+//When game restarts, make sure everything resets and start from step 1
+//Provide instructions on how to play
